8cf49619609520c80b1a5987126efe43fbd2bde7
Marco Salatin <marco.salatin@gmail.com>
2023-07-28 12:23:31 +0200

    wip

--- exercise/models.py
+++ exercise/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    fist_name = models.DateTimeField(null=False)
+    last_name = models.DateTimeField(null=False)
+    
+    class Meta:
+        abstract = True
+
+
+class Student(Person):
+    course = models.TextField(null=False)
+    hasScholarship = models.BooleanField(null=False)
+
+    class Meta:
+        db_table = "students"    
+
+
+class Teacher(Person):
+    qualification  = models.TextField(null=False)
+    salary = models.FloatField(null=False)
+
+    class Meta:
+        db_table = "teachers"    
+
