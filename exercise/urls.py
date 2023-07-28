2374765336facdb74fbcc7c078ec29d7331b4a09
Marco Salatin <marco.salatin@gmail.com>
2023-07-28 22:55:03 +0200

    wip

--- exercise/urls.py
+++ exercise/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
-    path("students", views.students, name="students")
+    path("students", views.students, name="students"),
+    path("doubleaverage", views.doubleaverage, name="doubleaverage")
+
 ]
--- exercise/views.py
+++ exercise/views.py
@@ -1,6 +1,8 @@
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render
 from .models import Student
+from django.db.models import F
+
 
 @require_http_methods(["GET"])
 def dashboard(request):
@@ -11,6 +13,15 @@ def students(request):
     if request.method == "POST":
         student = Student(first_name=request.POST['first_name'], last_name=request.POST['last_name'], course=request.POST['course'], average=request.POST['average'])
         student.save()
-        
+
+    students = Student.objects.all()
+    return render(request, "students.html", {'students': students})
+
+
+@require_http_methods(["GET"])
+def doubleaverage(request):
+    student = Student.objects.get(first_name="Mario")
+    student.average = F("average") * 2
+    student.save()
     students = Student.objects.all()
     return render(request, "students.html", {'students': students})
