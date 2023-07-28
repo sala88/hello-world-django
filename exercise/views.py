66e261f8fd0cea02a2b9f52c873784fa4687bcf8
Marco Salatin <marco.salatin@gmail.com>
2023-07-28 11:32:07 +0200

    wip

--- exercise/views.py
+++ exercise/views.py
@@ -1,7 +1,13 @@
-
-
+from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
+@require_http_methods(["GET"])
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world!")
+
+
+@require_http_methods(["GET"])
+def dashboard(request):
+    return render(request, "dashboard.html")
